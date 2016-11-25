208e47ab32b94bae035c466a07963d14db700d29
Warwick Booth <warwick@lsdev.biz>
2016-11-25 14:15:24 +0200

    Making sure the API

--- lsx-login.php
+++ lsx-login.php
@@ -14,6 +14,71 @@ License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2
 require 'inc/template-tags.php';
 require 'inc/class-login-widget.php';
 
+/* ======================= The API Classes ========================= */
+if(!class_exists('LSX_API_Manager')){
+	require_once('classes/class-lsx-api-manager.php');
+}
+
+/**
+ * Runs once when the plugin is activated.
+ */
+function lsx_login_activate_plugin() {
+	$lsx_to_password = get_option('lsx_api_instance',false);
+	if(false === $lsx_to_password){
+		update_option('lsx_api_instance',LSX_API_Manager::generatePassword());
+	}
+}
+register_activation_hook( __FILE__, 'lsx_login_activate_plugin' );
+
+/**
+ *	Grabs the email and api key from the LSX Login Settings.
+ */
+function lsx_login_options_pages_filter($pages){
+	$pages[] = 'lsx-settings';
+	$pages[] = 'to-settings';
+	return $pages;
+}
+add_filter('lsx_api_manager_options_pages','lsx_login_options_pages_filter',10,1);
+
+function lsx_login_api_admin_init(){
+	global $lsx_login_api_manager;
+	if(class_exists('Tour_Operator')) {
+		$options = get_option('_to_settings', false);
+	}else{
+		$options = get_option('_lsx_settings', false);
+		if (false === $options) {
+			$options = get_option('_lsx_lsx-settings', false);
+		}
+	}
+
+	$data = array('api_key'=>'','email'=>'');
+
+	if(false !== $options && isset($options['api'])){
+		if(isset($options['api']['lsx-login_api_key']) && '' !== $options['api']['lsx-login_api_key']){
+			$data['api_key'] = $options['api']['lsx-login_api_key'];
+		}
+		if(isset($options['api']['lsx-login_email']) && '' !== $options['api']['lsx-login_email']){
+			$data['email'] = $options['api']['lsx-login_email'];
+		}
+	}
+
+	$instance = get_option( 'lsx_api_instance', false );
+	if(false === $instance){
+		$instance = LSX_API_Manager::generatePassword();
+	}
+
+	$api_array = array(
+		'product_id'	=>		'LSX Login',
+		'version'		=>		'1.0.0',
+		'instance'		=>		$instance,
+		'email'			=>		$data['email'],
+		'api_key'		=>		$data['api_key'],
+		'file'			=>		'lsx-login.php'
+	);
+	$lsx_login_api_manager = new LSX_API_Manager($api_array);
+}
+add_action('admin_init','lsx_banners_api_admin_init');
+
 /**
  * Main plugin class.
  *
