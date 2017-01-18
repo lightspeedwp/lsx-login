624017fe8f9dbe532942db72d4ea735da9267ccd
Fernando Tessmann <nandotess85@gmail.com>
2017-01-18 08:28:27 -0200

    added NPM script to copy files to localhost servers (fernando tessmann localhost only)

---
+++ bin/deploy-localhost-nandotess.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "* NANDOTESS LOCALHOST DEPLOY *";
+cd ~/Sites-LightSpeed/@git/lsx-login;
+gulp compile-sass;
+gulp compile-js;
+gulp wordpress-lang;
+
+echo "* LSX.MAMP:8888 *";
+rm -Rf ~/Sites-LightSpeed/@mamp/lsx.mamp/wp-content/plugins/lsx-login;
+rsync -a \
+	--exclude='.git' \
+	--exclude='node_modules' \
+	--exclude='assets/css/lsx-login.scss' \
+	--exclude='.DS_Store' \
+	--exclude='.gitignore' \
+	--exclude='gulpfile.js' \
+	--exclude='package.json' \
+	~/Sites-LightSpeed/@git/lsx-login ~/Sites-LightSpeed/@mamp/lsx.mamp/wp-content/plugins;
+
+echo "* TSA-V2.MAMP:8888 *";
+rm -Rf ~/Sites-LightSpeed/@mamp/tsa-v2.mamp/wp-content/plugins/lsx-login;
+rsync -a \
+	--exclude='.git' \
+	--exclude='node_modules' \
+	--exclude='assets/css/lsx-login.scss' \
+	--exclude='.DS_Store' \
+	--exclude='.gitignore' \
+	--exclude='gulpfile.js' \
+	--exclude='package.json' \
+	~/Sites-LightSpeed/@git/lsx-login ~/Sites-LightSpeed/@mamp/tsa-v2.mamp/wp-content/plugins;
