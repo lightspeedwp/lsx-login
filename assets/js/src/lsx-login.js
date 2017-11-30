1c8a8290294a0da82622d08d12c43d391e4b0b6a
Fernando Tessmann <nandotess85@gmail.com>
2017-11-29 23:02:01 -0600

    * Skipping the enqueue of the select2 when woocommerce is active to avoid conflicts * NPM and Gulp files updated * Changelog updated * Plugin version increased

--- assets/js/src/lsx-login.js
+++ assets/js/src/lsx-login.js
@@ -3,40 +3,40 @@ jQuery(document).ready(function($) {
 
 	var isMobile = false; //initiate as false
 	// device detection
-	if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+	if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
 
 	if(false == isMobile){
 		jQuery('.loginform').submit(function(event){
-			
+
 			event.preventDefault();
 			var formObj = $(this);
-			
+
 			//remove all error and validation fields
 			$(this).find('div.error').each(function(event){
 				$(this).remove();
 			});
-			
-			
+
+
 			var username = $(this).find('input.user_login').val();
 			if('' == username){
 				$(this).find('input.user_login').parent('p').append('<div class="error">'+lsx_login_params.empty_username+'</div>');
                 $(this).find('input.user_login').addClass('error');
                 return false;
 			}
-			
+
 			var password = $(this).find('input.user_pass').val();
 			if('' == password){
 				$(this).find('input.user_pass').parent('p').append('<div class="error">'+lsx_login_params.empty_password+'</div>');
                 $(this).find('input.user_pass').addClass('error');
 				return false;
-			}		
-				
+			}
+
 			var remember = '';
 		    if ( $(this).find('input[name="rememberme"]:checked' ).size() > 0 ) {
 		    	remember = $(this).find('input[name="rememberme"]:checked').val();
 		    }
-		    
+
 		    var redirect = $(this).find('input[name="redirect_to"]' ).val();
 		    var params = {
 				action: 		'lsx_login',
@@ -44,12 +44,12 @@ jQuery(document).ready(function($) {
 				pwd:		 	password,
 				rememberme: 	remember,
 				method:			'login'
-			};	 
-		    
-		    
+			};
+
+
 		    //Gery out the form field
 		    $(this).addClass('loading');
-		    
+
 		    //Load the spinner
 			$(this).prepend('<img style="display:none;" src="'+lsx_login_params.ajax_spinner+'" class="spinner" />');
 			var adjustHeight = $(this).height()/2 - 64;
@@ -59,18 +59,18 @@ jQuery(document).ready(function($) {
 			$(this).find('.spinner').show();
 
 			var form_obj = $(this);
-			
+
 			$.ajax({
 				url: lsx_login_params.ajax_url,
 				data: params,
 				type: 'POST',
 				success: function( response ) {
-					
+
 					if(false != response){
 						var result = $.parseJSON( response );
-						
+
 						if(result.success == '1'){ window.location.href = redirect;  }
-						
+
 						if(result.success == '3'){
 							formObj.find('.login-password').append('<div class="error">'+result.message+'</div>');
                             formObj.find('.login-password input').addClass('error');
@@ -84,23 +84,23 @@ jQuery(document).ready(function($) {
 					}
 				}
 
-			});		
-			
+			});
+
 			return false;
 		});
-		
-		
+
+
 		jQuery('form[name="lostpasswordform"]').submit(function(event){
-			
+
 			event.preventDefault();
 			var formObj = $(this);
-			
+
 			//remove all error and validation fields
 			$(this).find('.error').each(function(event){
 				$(this).remove();
 			});
-			
-			
+
+
 			var username = $(this).find('input.user_login').val();
 			if('' == username){
 				$(this).find('input.user_login').parent('p').append('<div class="error">'+lsx_login_params.empty_reset+'</div>');
@@ -111,12 +111,12 @@ jQuery(document).ready(function($) {
 				action: 		'lsx_reset',
 				log:		 	username,
 				method:			'reset'
-			};	 
-		    
-		    
+			};
+
+
 		    //Gery out the form field
 		    $(this).addClass('loading');
-		    
+
 		    //Load the spinner
 			$(this).prepend('<img style="display:none;" src="'+lsx_login_params.ajax_spinner+'" class="spinner" />');
 			var adjustHeight = $(this).height()/2 - 64;
@@ -124,18 +124,18 @@ jQuery(document).ready(function($) {
 			$(this).find('.spinner').css('margin-top',adjustHeight+'px');
 			$(this).find('.spinner').css('margin-left',adjustWidth+'px');
 			$(this).find('.spinner').show();
-			
+
 			$.ajax({
 				url: lsx_login_params.ajax_url,
 				data: params,
 				type: 'POST',
 				success: function( response ) {
-					
+
 					if(false != response){
 						var result = $.parseJSON( response );
 						$('.spinner').remove();
 						$('.lostpasswordform.loading').removeClass('loading');
-						
+
 						if(result.success == '3'){
 							formObj.append('<p class="error">'+result.message+'</p>');
                             formObj.find('input.user_login').addClass('error');
@@ -145,98 +145,98 @@ jQuery(document).ready(function($) {
 						}else if(result.success == '1'){
 							formObj.find('p').each(function(event){
 								$(this).remove();
-							}); 
+							});
 							formObj.append('<p class="success">'+result.message+'</p>');
 						}
 					}
 				}
 
-			});		
-			
+			});
+
 			return false;
 		});
-		
-		
+
+
 		jQuery('form[name="resetpassform"]').submit(function(event){
 			event.preventDefault();
 			var formObj = $(this);
-			
+
 			//remove all error and validation fields
 			$(this).find('.error').each(function(event){
 				$(this).remove();
 			});
-			
-			
+
+
 			var pass1 = $(this).find('input.pass1').val();
 			if('' == pass1){
 				$(this).find('input.pass1').parent('p').append('<div class="error">'+lsx_login_params.empty_password+'</div>');
                 $(this).find('input.pass1').addClass('error');
 				return false;
-			}	
-			
+			}
+
 			var pass2 = $(this).find('input.pass2').val();
 			if('' == pass2){
 				$(this).find('input.pass2').parent('p').append('<div class="error">'+lsx_login_params.empty_password+'</div>');
                 $(this).find('input.pass2').addClass('error');
 				return false;
 			}
-			
+
 			if(pass1 !== pass2 ){
 				$(this).find('input.pass2').parent('p').append('<div class="error">'+lsx_login_params.no_match+'</div>');
                 $(this).find('input.pass2').addClass('error');
 				return false;
 			}
-			
+
 			var key = $(this).find('input[name="rp_key"]').val();
-			var login = $(this).find('input[name="user_login"]').val();		
-			
+			var login = $(this).find('input[name="user_login"]').val();
+
 		    var params = {
 					action: 		'lsx_reset_confirmed',
 					pass1:		 	pass1,
 					pass2:		 	pass2,
 					key:		 	key,
 					login:		 	login
-			};	 
-			    
-			    
+			};
+
+
 		    //Gery out the form field
 		    $(this).addClass('loading');
-		    
+
 		    //Load the spinner
 			$(this).prepend('<img style="display:none;" src="'+lsx_login_params.ajax_spinner+'" class="spinner" />');
 			var adjustHeight = $(this).height()/2 - 64;
 			var adjustWidth = $(this).height()/2 - 64;
 			$(this).find('.spinner').css('margin-top',adjustHeight+'px');
 			$(this).find('.spinner').css('margin-left',adjustWidth+'px');
-			$(this).find('.spinner').show();	
-			
+			$(this).find('.spinner').show();
+
 			$.ajax({
 				url: lsx_login_params.ajax_url,
 				data: params,
 				type: 'POST',
 				success: function( response ) {
-					
+
 					if(false != response){
 						var result = $.parseJSON( response );
 						$('.spinner').remove();
 						$('.lostpasswordform.loading').removeClass('loading');
-						
+
 						if(result.success == '2'){
 							formObj.append('<p class="error">'+result.message+'</p>');
                             formObj.find('input.pass2').addClass('error');
 						}else if(result.success == '1'){
 							formObj.find('p').each(function(event){
 								$(this).remove();
-							}); 
+							});
 							formObj.append('<p class="success">'+result.message+'</p>');
 						}
 					}
 				}
 
-			});	
-			
+			});
+
 		});
-		
+
 		$('input.user_login , input.user_pass, input.pass1, input.pass2').click(function(event){
             $(this).removeClass('error');
 			$(this).parent('p').find('.error').remove();
@@ -261,7 +261,7 @@ jQuery(document).ready(function($) {
 
 	if (('undefined' === typeof jQuery().dropdown)) {
 		jQuery.ajax({
-			url: lsx_login_params.theme_url + 'assets/js/bootstrap.dropdown.js',
+			url: lsx_login_params.theme_url + 'assets/js/vendor/bootstrap.dropdown.js',
 			dataType: "script",
 			cache: true
 		});
@@ -269,7 +269,7 @@ jQuery(document).ready(function($) {
 
 	if (('undefined' === typeof jQuery().modal)) {
 		jQuery.ajax({
-			url: lsx_login_params.theme_url + 'assets/js/bootstrap.modal.js',
+			url: lsx_login_params.theme_url + 'assets/js/vendor/bootstrap.modal.js',
 			dataType: "script",
 			cache: true
 		});
@@ -277,7 +277,7 @@ jQuery(document).ready(function($) {
 
 	if (('undefined' === typeof jQuery().tab)) {
 		jQuery.ajax({
-			url: lsx_login_params.theme_url + 'assets/js/bootstrap.tab.js',
+			url: lsx_login_params.theme_url + 'assets/js/vendor/bootstrap.tab.js',
 			dataType: "script",
 			cache: true
 		});
--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,148 @@
-var gulp = require('gulp');
+const gulp         = require('gulp');
+const rtlcss       = require('gulp-rtlcss');
+const sass         = require('gulp-sass');
+const sourcemaps   = require('gulp-sourcemaps');
+const jshint       = require('gulp-jshint');
+const concat       = require('gulp-concat');
+const uglify       = require('gulp-uglify');
+const sort         = require('gulp-sort');
+const wppot        = require('gulp-wp-pot');
+const gettext      = require('gulp-gettext');
+const plumber      = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const gutil        = require('gulp-util');
+const rename       = require('gulp-rename');
+const minify       = require('gulp-minify-css');
+const map          = require('map-stream');
+const browserlist  = ['last 2 version', '> 1%'];
 
-gulp.task('default', function() {	 
+const errorreporter = map(function(file, cb) {
+	if (file.jshint.success) {
+		return cb(null, file);
+	}
+
+	console.log('JSHINT fail in', file.path);
+
+	file.jshint.results.forEach(function (result) {
+		if (!result.error) {
+			return;
+		}
+
+		const err = result.error
+		console.log(`  line ${err.line}, col ${err.character}, code ${err.code}, ${err.reason}`);
+	});
+
+	cb(null, file);
+});
+
+gulp.task('default', function() {
 	console.log('Use the following commands');
 	console.log('--------------------------');
-	console.log('gulp sass           to compile the style.scss to style.css');
-	console.log('gulp compile-sass   to compile both of the above.');
-	console.log('gulp js             to compile the custom.js to custom.min.js');
-	console.log('gulp compile-js     to compile both of the above.');
-	console.log('gulp watch          to continue watching all files for changes, and build when changed');
-	console.log('gulp wordpress-lang to compile the lsx-login.pot');
+	console.log('gulp compile-css    to compile the scss to css');
+	console.log('gulp compile-js     to compile the js to min.js');
+	console.log('gulp watch          to continue watching the files for changes');
+	console.log('gulp wordpress-lang to compile the lsx-login.pot, en_EN.po and en_EN.mo');
 });
 
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var sort = require('gulp-sort');
-var wppot = require('gulp-wp-pot');
-
-gulp.task('sass', function () {
-    gulp.src('assets/css/lsx-login.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('assets/css/'));
+gulp.task('styles', function () {
+	return gulp.src('assets/css/scss/*.scss')
+		.pipe(plumber({
+			errorHandler: function(err) {
+				console.log(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(sourcemaps.init())
+		.pipe(sass({
+			outputStyle: 'compact',
+			includePaths: ['assets/css/scss']
+		}).on('error', gutil.log))
+		.pipe(autoprefixer({
+			browsers: browserlist,
+			casacade: true
+		}))
+		.pipe(sourcemaps.write('maps'))
+		.pipe(gulp.dest('assets/css'))
 });
 
-gulp.task('js', function () {
-	gulp.src('assets/js/lsx-login.js')
-	.pipe(concat('lsx-login.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('assets/js'));
+gulp.task('styles-rtl', function () {
+	return gulp.src('assets/css/scss/*.scss')
+		.pipe(plumber({
+			errorHandler: function(err) {
+				console.log(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(sass({
+			outputStyle: 'compact',
+			includePaths: ['assets/css/scss']
+		}).on('error', gutil.log))
+		.pipe(autoprefixer({
+			browsers: browserlist,
+			casacade: true
+		}))
+		.pipe(rtlcss())
+		.pipe(rename({
+			suffix: '-rtl'
+		}))
+		.pipe(gulp.dest('assets/css'))
 });
- 
-gulp.task('compile-sass', (['sass']));
-gulp.task('compile-js', (['js']));
 
-gulp.task('watch', function() {
-	gulp.watch('assets/css/lsx-login.scss', ['sass']);
-	gulp.watch('assets/js/lsx-login.js', ['js']);
+gulp.task('compile-css', ['styles', 'styles-rtl']);
+
+gulp.task('js', function() {
+	return gulp.src('assets/js/src/lsx-login.js')
+		.pipe(plumber({
+			errorHandler: function(err) {
+				console.log(err);
+				this.emit('end');
+			}
+		}))
+		.pipe(jshint())
+		//.pipe(errorreporter)
+		.pipe(concat('lsx-login.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('assets/js'))
 });
 
-gulp.task('wordpress-lang', function () {
+gulp.task('compile-js', ['js']);
+
+gulp.task('watch-css', function () {
+	return gulp.watch('assets/css/**/*.scss', ['compile-css']);
+});
+
+gulp.task('watch-js', function () {
+	return gulp.watch('assets/js/src/**/*.js', ['compile-js']);
+});
+
+gulp.task('watch', ['watch-css', 'watch-js']);
+
+gulp.task('wordpress-pot', function() {
 	return gulp.src('**/*.php')
 		.pipe(sort())
 		.pipe(wppot({
 			domain: 'lsx-login',
-			destFile: 'lsx-login.pot',
 			package: 'lsx-login',
-			bugReport: 'https://bitbucket.org/feedmycode/lsx-login/issues',
 			team: 'LightSpeed <webmaster@lsdev.biz>'
 		}))
-		.pipe(gulp.dest('languages'));
+		.pipe(gulp.dest('languages/lsx-login.pot'))
 });
+
+gulp.task('wordpress-po', function() {
+	return gulp.src('**/*.php')
+		.pipe(sort())
+		.pipe(wppot({
+			domain: 'lsx-login',
+			package: 'lsx-login',
+			team: 'LightSpeed <webmaster@lsdev.biz>'
+		}))
+		.pipe(gulp.dest('languages/en_EN.po'))
+});
+
+gulp.task('wordpress-po-mo', ['wordpress-po'], function() {
+	return gulp.src('languages/en_EN.po')
+		.pipe(gettext())
+		.pipe(gulp.dest('languages'))
+});
+
+gulp.task('wordpress-lang', (['wordpress-pot', 'wordpress-po-mo']));
