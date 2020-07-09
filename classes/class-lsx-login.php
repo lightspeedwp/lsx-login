7e55d740e0beb4471dbef87b1ff7f8693c02e3a0
Warwick Booth <warwick@lsdev.biz>
2020-07-09 13:32:48 +0200

    Removing the reference to the API tab

--- classes/class-lsx-login.php
+++ classes/class-lsx-login.php
@@ -569,18 +569,6 @@ if ( ! class_exists( 'LSX_Login' ) ) {
 				$default = false;
 			}
 
-			if ( ! array_key_exists( 'api', $tabs ) ) {
-				$tabs['api'] = array(
-					'page_title'        => '',
-					'page_description'  => '',
-					'menu_title'        => esc_html__( 'API', 'lsx-login' ),
-					'template'          => LSX_LOGIN_PATH . 'includes/settings/api.php',
-					'default'           => $default
-				);
-
-				$default = false;
-			}
-
 			return $tabs;
 		}
 
