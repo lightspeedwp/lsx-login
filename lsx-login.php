7cb54d58ff4fd4de8b6764cc08fa72aa972fc06d
Warwick <krugazul@gmail.com>
2024-01-09 12:21:37 +0200

    Stripping out the old code and updating the version values.

--- lsx-login.php
+++ lsx-login.php
@@ -3,7 +3,7 @@
  * Plugin Name: LSX Login
  * Plugin URI:  https://www.lsdev.biz/product/lsx-login/
  * Description:	The LSX Login extension allows users to log into a dashboard and then see configurable content based on which users can access which content.
- * Version:     1.0.5
+ * Version:     2.0.0
  * Author:      LightSpeed
  * Author URI:  https://www.lsdev.biz/
  * License:     GPL3
@@ -20,14 +20,8 @@ if ( ! defined( 'WPINC' ) ) {
 define('LSX_LOGIN_PATH', plugin_dir_path( __FILE__ ) );
 define('LSX_LOGIN_CORE', __FILE__ );
 define('LSX_LOGIN_URL',  plugin_dir_url( __FILE__ ) );
-define('LSX_LOGIN_VER',  '1.0.5' );
-
-if(!function_exists('cmb_init')){
-	if (is_file(LSX_LOGIN_PATH.'vendor/Custom-Meta-Boxes/custom-meta-boxes.php')) {
-		require LSX_LOGIN_PATH.'vendor/Custom-Meta-Boxes/custom-meta-boxes.php';
-	}
-}
+define('LSX_LOGIN_VER',  '2.0.0' );
 
 /* ======================= Below is the Plugin Class init ========================= */
 
-require_once( LSX_LOGIN_PATH . 'classes/class-lsx-login.php' );
+require_once( LSX_LOGIN_PATH . 'classes/class-core.php' );
