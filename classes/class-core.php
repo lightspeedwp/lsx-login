bde24f1f5ebda2dbd9d81258d2faff4664c93d22
Warwick <krugazul@gmail.com>
2024-01-09 12:32:39 +0200

    Fixing the login activation

---
+++ classes/class-core.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The core file
+ *
+ * @package LSX\Login
+ */
+
+namespace LSX\Login;
+
+/**
+ * The main file loading the rest of the files
+ *
+ * @package   FISA_Enhancements
+ * @author    LightSpeed
+ * @license   GPL3
+ * @link
+ * @copyright 2023 LightSpeed
+ */
+class Core {
+
+	/**
+	 * Contructor
+	 */
+	public function __construct() {
+	}
+
+	/**
+	 * Loads the actions we need.
+	 *
+	 * @return void
+	 */
+	public function init() {
+	}
+
+	/**
+	 * Loads the classes
+	 */
+	public function load_classes() {
+	}
+
+	/**
+	 * Loads the classes
+	 */
+	public function load_includes() {
+	}
+}
--- lsx-login.php
+++ lsx-login.php
@@ -25,3 +25,18 @@ define('LSX_LOGIN_VER',  '2.0.0' );
 /* ======================= Below is the Plugin Class init ========================= */
 
 require_once( LSX_LOGIN_PATH . 'classes/class-core.php' );
+
+/**
+ * Returns and instance of the LSX Sharing plugin.
+ *
+ * @return object \lsx\sharing\Sharing();
+ */
+function lsx_login() {
+	global $lsx_login;
+	if ( null === $lsx_login ) {
+		$lsx_login = new \LSX\Login\Core();
+		$lsx_login->init();
+	}
+    return $lsx_login;
+}
+add_action( 'plugins_loaded', 'lsx_login' );
